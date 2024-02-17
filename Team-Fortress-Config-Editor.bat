a37f20ae7fb86c94dcac69a50f168d019303dccc
Ivan <92064752+UltraToon@users.noreply.github.com>
2024-02-16 20:21:15 -0800

    Update and rename TeamFortressConfigEditor.bat to Team-Fortress-Config-Editor.bat

--- Team-Fortress-Config-Editor.bat
+++ Team-Fortress-Config-Editor.bat
@@ -8,15 +8,12 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-:: Define variables
 set "repoURL=https://raw.githubusercontent.com/UltraToon/Team-Fortress-Config-Editor/main"
-set "scriptName=TeamFortressConfigEditor.ps1"
+set "scriptName=main.ps1"
 
-:: Download the latest script
 echo Downloading %scriptName%...
 powershell -executionpolicy bypass -command "& { Invoke-Expression ((New-Object Net.WebClient).DownloadString('%repoURL%/%scriptName%')) }"
 
-:: Check if the download was successful
 if %errorlevel% neq 0 (
     echo Failed to download/execute %scriptName%. Try again later!
     echo Open an issue: https://github.com/UltraToon/Team-Fortress-Config-Editor/issues
