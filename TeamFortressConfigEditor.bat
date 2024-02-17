b051302cf38e5d3fee14a904401fd5047e44ebf2
Ivan <92064752+UltraToon@users.noreply.github.com>
2024-02-16 20:19:13 -0800

    Add files via upload

--- TeamFortressConfigEditor.bat
+++ TeamFortressConfigEditor.bat
@@ -1,29 +1,25 @@
 @echo off
 setlocal enabledelayedexpansion
 
+ver | find "10." > nul
+if %errorlevel% neq 0 (
+    echo Windows 10 and above only, please upgrade!
+    pause
+    exit /b 1
+)
+
 :: Define variables
 set "repoURL=https://raw.githubusercontent.com/UltraToon/Team-Fortress-Config-Editor/main"
 set "scriptName=TeamFortressConfigEditor.ps1"
 
 :: Download the latest script
 echo Downloading %scriptName%...
-powershell -command "& { Invoke-WebRequest -Uri '%repoURL%/%scriptName%' -OutFile '%scriptName%' }"
+powershell -executionpolicy bypass -command "& { Invoke-Expression ((New-Object Net.WebClient).DownloadString('%repoURL%/%scriptName%')) }"
 
 :: Check if the download was successful
 if %errorlevel% neq 0 (
-    echo Failed to download %scriptName%.
+    echo Failed to download/execute %scriptName%. Try again later!
+    echo Open an issue: https://github.com/UltraToon/Team-Fortress-Config-Editor/issues
     pause
     exit /b 1
 )
-
-:: Execute the downloaded script
-echo Running %scriptName%...
-start powershell -executionpolicy bypass -file "%scriptName%"
-
-:: Check if the execution was successful
-if %errorlevel% neq 0 (
-    echo Failed to run %scriptName%.
-    pause
-    exit /b 1
-)
-exit /b 0
