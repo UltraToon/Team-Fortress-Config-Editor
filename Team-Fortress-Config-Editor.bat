e2400b339907707c17a98861361e361ddfc17abb
Ivan <92064752+UltraToon@users.noreply.github.com>
2024-02-16 20:31:26 -0800

    Update Team-Fortress-Config-Editor.bat

--- Team-Fortress-Config-Editor.bat
+++ Team-Fortress-Config-Editor.bat
@@ -8,14 +8,11 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-set "repoURL=https://raw.githubusercontent.com/UltraToon/Team-Fortress-Config-Editor/main"
-set "scriptName=main.ps1"
-
 echo Downloading %scriptName%...
-powershell -executionpolicy bypass -command "& { Invoke-Expression ((New-Object Net.WebClient).DownloadString('%repoURL%/%scriptName%')) }"
+powershell -executionpolicy bypass -command "& { Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/UltraToon/Team-Fortress-Config-Editor/main/main.ps1')) }"
 
 if %errorlevel% neq 0 (
-    echo Failed to download/execute %scriptName%. Try again later!
+    echo Failed to download/execute main.ps1 (Team Fortress Config Editor). Try again later!
     echo Open an issue: https://github.com/UltraToon/Team-Fortress-Config-Editor/issues
     pause
     exit /b 1
